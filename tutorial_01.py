7cea150045965c0b202c8a3f6e657048ffd4d1c1
HsinYi <11050034@me.mcu.edu.tw>
2024-01-15 11:13:29 +0800

    儲存圖片

--- tutorial_01.py
+++ tutorial_01.py
@@ -3,14 +3,13 @@ import cv2
 img = cv2.imread('images\\traffic_light.png') 
 
 img = cv2.resize(img, (0,0),fx=2,fy=2)
-#圖片大小調整，以倍數做縮放
-"""
-(參數1:想要改變大小的圖片
-參數2:
-參數3:寬度要調成的倍數
-參數4:高度要調成的倍數)
-"""
 
 cv2.imshow('img',img) 
 
 cv2.waitKey(0) 
+
+cv2.imwrite('images\\img2.png',img)
+"""
+儲存圖片
+("要存到的路徑檔名"，圖片)
+"""
