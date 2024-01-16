c05631fd2f297152ff2d76cad564ad348c304d50
HsinYi <11050034@me.mcu.edu.tw>
2024-01-16 10:49:01 +0800

    取得圖片的邊緣(描邊)

--- 0116.py
+++ 0116.py
@@ -4,18 +4,21 @@ img = cv.imread("images\\traffic_light.png")
 
 img = cv.resize(img, (0,0), fx=2, fy=2)
 
-blur = cv.GaussianBlur(img, (9,9), 10)
+canny = cv.Canny(img, 500, 550)
 '''
-高斯模糊
-(要轉換的圖片，和: 只能是奇數，標準差)
-調整和跟標準差可以影響模糊效果，數字越高越糊
+取得圖片的邊緣: 想成描邊的意思比較好理解
+(要處理的圖片，最低門檻值:過濾掉，最高門檻值:當邊緣看)
+基於像素值差異所造成的效果
+在每個像素點打分，和旁邊的像素質差越多分越高
+門檻值就是分數
+把值放越低輪廓就越明顯
 '''
 
 cv.imshow('img', img)
 
-cv.imshow('blur', blur)
+cv.imshow('canny', canny)
 
 cv.waitKey(0)
 
 cv.imwrite('images\\img_0116_original.png',img)
-cv.imwrite('images\\img_0116_new.png',blur)
+cv.imwrite('images\\img_0116_new.png',canny)
