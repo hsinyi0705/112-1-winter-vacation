cbfe10a962ffae54f7ba2550fab27744abbe13d9
HsinYi <11050034@me.mcu.edu.tw>
2024-01-16 14:15:26 +0800

    建立BGR圖像(陣列方式) : 改寫陣列元素

--- 0116_2.py
+++ 0116_2.py
@@ -2,24 +2,24 @@ import cv2 as cv
 import numpy as np
 import random
 
-hight = 160
-width = 280
+hight = 150
+width = 300
 
-#建立BGR圖像(陣列方式)
-#先使用zeros建立一全為0的陣列(三通道)，再修改其中欲產生的圖片顏色的通道
 img1 = np.zeros((hight,width,3), np.uint8)
-img1[:,:,0] = 255 #B通道
-img2 = np.zeros((hight,width,3), np.uint8)
-img2[:,:,1] = 255 #G通道
-img3 = np.zeros((hight,width,3), np.uint8)
-img3[:,:,2] = 255 #R通道
+'''
+[參數1:寬，參數2:為所有，參數3:顏色通道]
+
+陣列元素:
+同一參數直接使用 ':' 代表 '到'
+單一參數只有 ':' 代表該元素整體
+'''
+img1[0:50, :, 0] = 255  
+img1[50:100, :, 1] = 255
+img1[100:150, :, 2] = 255 
 
 
 cv.imshow("img1",img1)
-cv.imshow("img2",img2)
-cv.imshow("img3",img3)
 cv.waitKey(0)
 
 cv.imwrite('images\\img_0116_431.png',img1)
-cv.imwrite('images\\img_0116_432.png',img2)
-cv.imwrite('images\\img_0116_433.png',img3)
+
