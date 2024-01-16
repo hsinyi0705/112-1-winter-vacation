327122b1bd9838bb0637b6ccf846d2e8bfd4e076
HsinYi <11050034@me.mcu.edu.tw>
2024-01-16 13:38:41 +0800

    以陣列方式建立圖片(灰階) : random

--- 0116_2.py
+++ 0116_2.py
@@ -1,21 +1,24 @@
 import cv2 as cv
 import numpy as np
+import random
 
 hight = 160
 width = 280
 
 #以陣列方式建立圖片(灰階)
-img = np.ones((hight,width), np.uint8)*255
+img1 = np.random.randint(256, size=[hight,width], dtype=np.uint8)
+img2 = np.random.randint(256, size=[hight,width,3], dtype=np.uint8)
 '''
-.ones((高，寬)，編碼)
+randint(隨機數最大值，size=[高,寬,維度]，dtype=編碼)
 
-一般opencv的圖片編碼都為uint8，
-ones可以將陣列中所有元素都填入1，
-再利用*255，
-使圖片為白色。
+回傳一個以隨機數填滿各元素的的陣列，
+維度若不填寫為灰階圖，
+填則3為BGR圖
 '''
 
-cv.imshow("img",img)
+cv.imshow("img1",img1)
+cv.imshow("img2",img2)
 cv.waitKey(0)
 
-cv.imwrite('images\\img_0116_2.png',img)
+cv.imwrite('images\\img_0116_41.png',img1)
+cv.imwrite('images\\img_0116_42.png',img2)
