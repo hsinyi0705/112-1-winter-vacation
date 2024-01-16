f0add69b5a3e1b6cb89f228ececf35322ad44dfa
HsinYi <11050034@me.mcu.edu.tw>
2024-01-16 10:36:27 +0800

    將圖片模糊處理

--- 0116.py
+++ 0116.py
@@ -4,15 +4,18 @@ img = cv.imread("images\\traffic_light.png")
 
 img = cv.resize(img, (0,0), fx=2, fy=2)
 
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+blur = cv.GaussianBlur(img, (9,9), 10)
 '''
-(要轉換的圖片，COLOR_BGR2GRAY: BGR 轉成 GRAY)
+高斯模糊
+(要轉換的圖片，和: 只能是奇數，標準差)
+調整和跟標準差可以影響模糊效果，數字越高越糊
 '''
 
-#cv.imshow('img', img)
+cv.imshow('img', img)
 
-cv.imshow('gray', gray)
+cv.imshow('blur', blur)
 
 cv.waitKey(0)
 
-cv.imwrite('images\\img_0116.png',gray)
+cv.imwrite('images\\img_0116_original.png',img)
+cv.imwrite('images\\img_0116_new.png',blur)
