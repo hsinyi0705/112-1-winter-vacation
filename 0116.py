55e6a8bed4e42d8d01141c2117a06a3b31380f68
HsinYi <11050034@me.mcu.edu.tw>
2024-01-16 10:58:47 +0800

    將圖片轉換成 HSV(色彩空間)

--- 0116.py
+++ 0116.py
@@ -2,23 +2,19 @@ import cv2 as cv
 
 img = cv.imread("images\\traffic_light.png")
 
-img = cv.resize(img, (0,0), fx=2, fy=2)
+img = cv.resize(img, (0,0), fx=1.5, fy=1.5)
 
-canny = cv.Canny(img, 500, 550)
+hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 '''
-取得圖片的邊緣: 想成描邊的意思比較好理解
-(要處理的圖片，最低門檻值:過濾掉，最高門檻值:當邊緣看)
-基於像素值差異所造成的效果
-在每個像素點打分，和旁邊的像素質差越多分越高
-門檻值就是分數
-把值放越低輪廓就越明顯
+BGR 轉 HSV
+HSV: 在 HSV 色彩空間下，比BGR 更容易追蹤某種顏色的物體，常用於分割指定顏色的物體。
 '''
 
 cv.imshow('img', img)
 
-cv.imshow('canny', canny)
+cv.imshow('hsv', hsv)
 
 cv.waitKey(0)
 
 cv.imwrite('images\\img_0116_original.png',img)
-cv.imwrite('images\\img_0116_new.png',canny)
+cv.imwrite('images\\img_0116_new.png',hsv)
