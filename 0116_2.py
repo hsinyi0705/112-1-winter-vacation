8d5622723ea9350a7ce9eee2b3803160b7721906
HsinYi <11050034@me.mcu.edu.tw>
2024-01-16 13:46:01 +0800

    建立BGR圖像(陣列方式)

--- 0116_2.py
+++ 0116_2.py
@@ -5,20 +5,21 @@ import random
 hight = 160
 width = 280
 
-#以陣列方式建立圖片(灰階)
-img1 = np.random.randint(256, size=[hight,width], dtype=np.uint8)
-img2 = np.random.randint(256, size=[hight,width,3], dtype=np.uint8)
-'''
-randint(隨機數最大值，size=[高,寬,維度]，dtype=編碼)
+#建立BGR圖像(陣列方式)
+#先使用zeros建立一全為0的陣列(三通道)，再修改其中欲產生的圖片顏色的通道
+img1 = np.zeros((hight,width,3), np.uint8)
+img1[:,:,0] = 255 #B通道
+img2 = np.zeros((hight,width,3), np.uint8)
+img2[:,:,1] = 255 #G通道
+img3 = np.zeros((hight,width,3), np.uint8)
+img3[:,:,2] = 255 #R通道
 
-回傳一個以隨機數填滿各元素的的陣列，
-維度若不填寫為灰階圖，
-填則3為BGR圖
-'''
 
 cv.imshow("img1",img1)
 cv.imshow("img2",img2)
+cv.imshow("img3",img3)
 cv.waitKey(0)
 
-cv.imwrite('images\\img_0116_41.png',img1)
-cv.imwrite('images\\img_0116_42.png',img2)
+cv.imwrite('images\\img_0116_431.png',img1)
+cv.imwrite('images\\img_0116_432.png',img2)
+cv.imwrite('images\\img_0116_433.png',img3)
