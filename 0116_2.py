8189d135afe9fe8a4846ce81f345420cdd6fcce7
HsinYi <11050034@me.mcu.edu.tw>
2024-01-16 13:33:49 +0800

    以陣列方式建立圖片(灰階) : 全白圖片

--- 0116_2.py
+++ 0116_2.py
@@ -5,13 +5,14 @@ hight = 160
 width = 280
 
 #以陣列方式建立圖片(灰階)
-img = np.zeros((hight,width), np.uint8)
+img = np.ones((hight,width), np.uint8)*255
 '''
-.zeros((高，寬)，編碼)
+.ones((高，寬)，編碼)
 
 一般opencv的圖片編碼都為uint8，
-zeros可以將陣列中所有元素都填入0，
-使圖片為黑色。
+ones可以將陣列中所有元素都填入1，
+再利用*255，
+使圖片為白色。
 '''
 
 cv.imshow("img",img)
