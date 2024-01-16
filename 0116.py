4b19bb859254f3185ce519ac7eb6fb7f9aad6650
HsinYi <11050034@me.mcu.edu.tw>
2024-01-16 11:12:09 +0800

    使用 merge() 合併色彩通道

--- 0116.py
+++ 0116.py
@@ -9,15 +9,17 @@ img = cv.resize(img, (0,0), fx=0.5, fy=0.5)
 cv.imshow('img', img)
 cv.waitKey(0)
 
-#使用split()拆解色彩通道
-b,g,r = cv.split(img)
 
+b,g,r = cv.split(img)
 cv.imshow('b',b)
 cv.imshow('g',g)
 cv.imshow('r',r)
 cv.waitKey(0)
 
-cv.imwrite('images\\img_0116_original.png',img)
-cv.imwrite('images\\img_0116_new-b.png',b)
-cv.imwrite('images\\img_0116_new-g.png',g)
-cv.imwrite('images\\img_0116_new-r.png',r)
+#使用 merge() 合併色彩通道
+#merge([藍,綠,紅])，回傳值三色彩通道的圖片
+img_merge = cv.merge([b,g,r])
+cv.imshow('merge',img_merge)
+cv.waitKey(0)
+
+cv.imwrite('images\\img_0116_new.png',img_merge)
