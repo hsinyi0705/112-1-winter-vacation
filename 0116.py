9e032463058dd565b2647b881297b05e2dadc734
HsinYi <11050034@me.mcu.edu.tw>
2024-01-16 11:00:57 +0800

    將圖片轉換成 RGB(色彩空間)

--- 0116.py
+++ 0116.py
@@ -2,19 +2,18 @@ import cv2 as cv
 
 img = cv.imread("images\\traffic_light.png")
 
-img = cv.resize(img, (0,0), fx=1.5, fy=1.5)
+img = cv.resize(img, (0,0), fx=2, fy=2)
 
-hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 '''
-BGR 轉 HSV
-HSV: 在 HSV 色彩空間下，比BGR 更容易追蹤某種顏色的物體，常用於分割指定顏色的物體。
+BGR 轉 RGB
 '''
 
 cv.imshow('img', img)
 
-cv.imshow('hsv', hsv)
+cv.imshow('rgb', rgb)
 
 cv.waitKey(0)
 
 cv.imwrite('images\\img_0116_original.png',img)
-cv.imwrite('images\\img_0116_new.png',hsv)
+cv.imwrite('images\\img_0116_new.png',rgb)
