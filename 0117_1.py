b4b266bab54e0f8ae3cdd3e0f5a920f79ab8aa6d
HsinYi <11050034@me.mcu.edu.tw>
2024-01-17 11:03:18 +0800

    利用 for 迴圈去跑輪廓 ( 印出輪廓點、畫出輪廓 )

--- 0117_1.py
+++ 0117_1.py
@@ -5,6 +5,8 @@ img = cv.imread('images\\i2.jpg')
 # 0 - 將圖片放大方便操作
 img = cv.resize(img, (0,0), fx=2, fy=2)
 
+imgContour = img.copy() # 複製圖片
+
 # 1 - 轉成灰階 (因為在檢測輪廓上不需要用到顏色)
 img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
@@ -24,13 +26,29 @@ contours, hierarchy = cv.findContours(canny,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_NON
 -2 階層 (這邊用不到)
 '''
 
+# 4 - 利用 for 迴圈去跑輪廓
+for cnt in contours:
+    # 印出輪廓點(這些點連起來就是輪廓)
+    print(cnt) 
 
-
+    # 畫出輪廓
+    cv.drawContours(imgContour, cnt, -1, (255,0,0), 4)
+    '''
+    .drawContours
+    (參數1: 要畫在什麼圖形上
+    參數2: 要畫的輪廓
+    參數3: 要畫第幾個輪廓，-1是指全畫
+    參數4: 要用什麼顏色畫
+    參數5: 畫的線條粗度)
+    '''
 
 
 cv.imshow('img', img)
 cv.imshow('canny', canny)
+cv.imshow('imgContour', imgContour)
 cv.waitKey(0)
 
 cv.imwrite('images\\img0117-1.png',img)
 cv.imwrite('images\\img0117-2.png',canny)
+cv.imwrite('images\\img0117-3.png',imgContour)
+
