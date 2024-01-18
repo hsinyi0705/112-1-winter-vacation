b6afd768b82c6c7350a77dd8b56a31d6bedd4ceb
HsinYi <11050034@me.mcu.edu.tw>
2024-01-18 09:50:28 +0800

    小專案實作: 設定強行終止影片播放按鍵

--- final.py
+++ final.py
@@ -3,16 +3,19 @@ import cv2 as cv
 # 讀入影片檔案
 cap = cv.VideoCapture("images\\f1.mp4")
 
-
 while True:
     ret, frame = cap.read()
     '''
     ret: 看影片的下一幀有沒有讀取成功(ture or false)
     frame: 影片的下一幀畫面
     '''
+
     if ret:
         cv.imshow('video', frame)
     else:
         break
-    cv.waitKey(3)
+
+    if (cv.waitKey(3) == ord('q')): # 設定強行終止影片播放按鍵
+        break
+    
 
