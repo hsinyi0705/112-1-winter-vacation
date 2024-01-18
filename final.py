523bd100de0fbb908c38a9059b79ba4a71f1ffff
HsinYi <11050034@me.mcu.edu.tw>
2024-01-18 09:45:41 +0800

    小專案實作: 影片播放

--- final.py
+++ final.py
@@ -3,14 +3,16 @@ import cv2 as cv
 # 讀入影片檔案
 cap = cv.VideoCapture("images\\f1.mp4")
 
-ret, frame = cap.read()
-'''
-ret: 看影片的下一幀有沒有讀取成功(ture or false)
-frame: 影片的下一幀畫面
-'''
 
-if ret:
-    cv.imshow('video', frame)
-
-cv.waitKey(0)
+while True:
+    ret, frame = cap.read()
+    '''
+    ret: 看影片的下一幀有沒有讀取成功(ture or false)
+    frame: 影片的下一幀畫面
+    '''
+    if ret:
+        cv.imshow('video', frame)
+    else:
+        break
+    cv.waitKey(3)
 
